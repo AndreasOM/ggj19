98e8492a1dae79ee6210147097a0a54e0bfb2fde
Andreas Neukoetter <andreas@omni-mad.com>
2019-01-25 20:39:58 +0100

    + Added initial empty window.

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+
+extern crate minifb;
+
+use minifb::{Key, WindowOptions, Window};
+
+const WIDTH: usize = 1920;
+const HEIGHT: usize = 1080;
+
+fn main() {
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new("Test - ESC to exit",
+                                 WIDTH,
+                                 HEIGHT,
+                                 WindowOptions::default()).unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0; // write something more funny here!
+        }
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window.update_with_buffer(&buffer).unwrap();
+    }	
+}
